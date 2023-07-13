5e25dcedd894a24977e316d6aca6877adc900163
Esteban Freri <estebanfreri@icloud.com>
2023-07-13 12:11:16 +0200

    actualizacion

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const titulo = document.getElementById("textSupport")
 
 class Cliente  {
-    constructor (nombre,email,mensaje){
+    constructor (nombre,apellido,email,mensaje){
     this.nombre = nombre;
+    this.apellido = apellido;
     this.email = email;
     this.consulta = mensaje;
 }
@@ -14,6 +15,7 @@ const formulario = document.getElementById ("formulario")
 formulario.addEventListener ("submit", (e) => {
     e.preventDefault();
     const nombre = document.getElementById ("nombre");
+    const apellido = document.getElementById ("apellido");
     const email = document.getElementById ("email");
     const mensaje = document.getElementById ("mensaje");
     console.log("El nombre ingresado es: " + nombre.value);
@@ -21,7 +23,7 @@ formulario.addEventListener ("submit", (e) => {
     console.log("El email ingresado es: " + email.value);
     console.log("La observacion ingresada es: " + mensaje.value);
 
-const cliente = new Cliente (nombre.value,email.value,mensaje.value);
+const cliente = new Cliente (nombre.value,apellido.value,email.value,mensaje.value);
 arrayClientes.push(cliente);
 console.log(arrayClientes);
     formulario.reset();
